cf3f7e7853a3f28bf38fdb43dd2c01a6611e6c97
ThePhong1803 <112880913+ThePhong1803@users.noreply.github.com>
2022-10-04 14:04:46 +0700

    lab2_ex1

---
+++ Lab2/Core/Inc/SevenSegmentDisplay.h
@@ -0,0 +1,15 @@
+/*
+ * SevenSegmentDisplay.h
+ *
+ *  Created on: Oct 4, 2022
+ *      Author: PC
+ */
+
+#ifndef INC_SEVENSEGMENTDISPLAY_H_
+#define INC_SEVENSEGMENTDISPLAY_H_
+
+#include "main.h"
+
+void display7SEG(int num);
+
+#endif /* INC_SEVENSEGMENTDISPLAY_H_ */
