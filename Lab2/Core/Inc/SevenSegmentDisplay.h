870f1d480e47767ef5dacd7f5d1296676e763570
ThePhong1803 <112880913+ThePhong1803@users.noreply.github.com>
2022-10-04 14:34:43 +0700

    'lab2_ex5'

--- Lab2/Core/Inc/SevenSegmentDisplay.h
+++ Lab2/Core/Inc/SevenSegmentDisplay.h
@@ -14,7 +14,12 @@ extern const int MAX_LED;
 extern int index_led;
 extern int led_buffer[4];
 
+extern int hour;
+extern int minute;
+extern int second;
+
 void display7SEG(int num);
 void update7SEG(int index);
+void updateClockBuffer();
 
 #endif /* INC_SEVENSEGMENTDISPLAY_H_ */
--- Lab2/Core/Src/main.c
+++ Lab2/Core/Src/main.c
@@ -103,11 +103,24 @@ int main(void)
 
   HAL_GPIO_WritePin(DOT_GPIO_Port, DOT_Pin, RESET);
 
-
   while (1)
   {
     /* USER CODE END WHILE */
+	  second++;
+	  if(second >= 60){
+		  second = 0;
+		  minute++;
+	  }
+	  if(minute >= 60){
+		  minute = 0;
+		  hour++;
+	  }
+	  if(hour >= 24){
+		  hour = 0;
+	  }
 
+	  updateClockBuffer();
+	  HAL_Delay(1000);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
